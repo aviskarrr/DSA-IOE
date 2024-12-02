c13f32e517cf6b670ba6d826dc2194a6f6129aff
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2024-12-02 13:21:12 +0545

    lab-1-stack

--- Lab01(Stack)/stackinC.c
+++ Lab01(Stack)/stackinC.c
@@ -3,85 +3,90 @@
 #include<stdlib.h>
 #define MAXSIZE 10
 int stack[MAXSIZE];
-int top =-1;
+int top = -1;
 
 bool isEmpty(){
-    if(top ==-1){
+    if(top == -1){
         return true;
     }
     else{
         return false;
     }
 }
-bool isFull(){
-    if(top ==MAXSIZE){
-        return true;
-    }
-    else{
-        return false;
-    }
 
+bool isFull(){
+    if(top == MAXSIZE - 1){  // Corrected condition
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
 void push(int num){
     if(!isFull()){
-        top+=1;
-        stack[top]=num;
+        top += 1;
+        stack[top] = num;
+        printf("Pushed %d successfully!\n", num);
     }
     else{
-        printf("Couldnt insert, check if the stack is full!\n");
+        printf("Couldn't insert, stack is full!\n");
     }
-
 }
+
 int pop(){
     int num;
     if(!isEmpty()){
-        top-=1;
-        num= stack[top];
+        num = stack[top];  // Get the top item BEFORE decrementing
+        top -= 1;
+        printf("Popped %d\n", num);
         return num;
     }
     else{
-        printf("Couldnt insert, check if the stack is empty!\n");
+        printf("Couldn't pop, stack is empty!\n");
         return -1;
     }
 }
 
 int main(){
-
-
-    int choice;
-    int num;
-        printf("What do you want to do?\n");
+    int choice, num;
+    
+    while (1)
+    {
+        printf("\nStack Operations Menu:\n");
         printf("1. Push (max limit is 10 datas)\n");
         printf("2. Pop (max limit is 10 datas)\n");
         printf("3. Display\n");
         printf("4. Exit\n");
-        scanf("%d",&choice);
-    while (1)
-    {
-        printf("What do you want to do?\n");
-        scanf("%d",&choice);
-
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
             printf("Enter the number you want to push: ");
-            scanf("%d",&num);
+            scanf("%d", &num);
             push(num);
             break;
         case 2:
             pop();
             break;
         case 3:
-            for(int i=0;i<=top;i++){
-                printf("%d\n",stack[i]);
+            if (isEmpty()) {
+                printf("Stack is empty!\n");
+            } else {
+                printf("Stack contents:\n");
+                for(int i = 0; i <= top; i++){
+                    printf("%d ", stack[i]);
+                }
+                printf("\n");
             }
             break;
         case 4:
             exit(0);
         
         default:
-            break;
+            printf("Invalid choice. Please try again.\n");
         }
     }
     return 0;
