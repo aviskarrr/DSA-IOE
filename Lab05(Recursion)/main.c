1f03df8318bc151b70e028662c572f6d5deb79b6
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2025-01-27 10:46:41 +0545

    recursion

--- Lab05(Recursion)/main.c
+++ Lab05(Recursion)/main.c
@@ -11,10 +11,41 @@ int factorial(int n){
     }
 }
 
+
+int fibo(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1||n==2){
+        return 1;
+    }
+    else{
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+
+
+int toh(int n, char A, char B, char C){
+    if(n>0){
+        toh(n-1,A,C,B);
+        printf("Disk moved from %c to % c\n", A, C);
+        toh(n-1, B, A, C);
+    }
+    else{
+        return 0;
+    }
+}
+
+
 int main(){
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
     printf("Factorial of %d is %d\n", n, factorial(n));
+    printf("Fibonacci of %d is %d\n", n, fibo(n));
+
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+    toh(n, 'A', 'B', 'C');
     return 0;
 }
